252e42c0e3ec7558a7be8ae3a2404fc767c21ac8
twisted rainbow on fire <43458794+x1sts@users.noreply.github.com>
2022-12-05 11:40:15 +1000

    fix shared persistant text

--- app/src/main/java/com/psymblyb/noter/MainActivity.java
+++ app/src/main/java/com/psymblyb/noter/MainActivity.java
@@ -8,26 +8,42 @@ current:
 save file,  shared ->  save to, open directly use uri to save the file.
 read, char instead to maintain carriage return
 add widget code
+
+issues:
+uri has rando text in front of it /document/raw: string/byte array change
+manual permission grant, intent or requestpermiss
+get intent object for testing, objectinputstream
+share load or append choice
  */
 
 package com.psymblyb.noter;
 
+import static android.os.Environment.getExternalStorageDirectory;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.InputStreamReader;
+
 
 public class MainActivity extends AppCompatActivity {
     private static String SharedText = "";
     private static String action;
     private static String type;
+    private static Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,11 +63,15 @@ public class MainActivity extends AppCompatActivity {
         // if opened directly
         if(Intent.ACTION_VIEW.equals(action)) {
             try {
-                Uri uri = intent.getData();
+                uri = intent.getData();
                 BufferedReader br = new BufferedReader(new
                 InputStreamReader(getContentResolver().openInputStream(uri), "UTF-8"));
-                String Line;
-                while( (Line = br.readLine()) != null) SharedText = SharedText + Line;
+                int I;
+                while( (I = br.read() ) != -1) {
+                    SharedText = SharedText + (char)I;
+                }
+//                String Line;
+//                while( (Line = br.readLine()) != null) SharedText = SharedText + Line;
             } catch (Exception e) {
                 Log.d("Noter open view", e.getLocalizedMessage()); }
         }
@@ -75,9 +95,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         Log.d("Noter Final null", String.valueOf(SharedText==null));
-        if (SharedText != null) MEdit.setText(SharedText); // set to textbox
+        if (SharedText != null)
+        {
+            MEdit.setText(SharedText); // set to textbox
+            SharedText = null;
+        }
     }
 
+    //mostly just to get rid of the warnings
     private static void print() {
         Log.i("Noter Shared text", SharedText);
         Log.i("Noter action", action);
@@ -92,5 +117,20 @@ public class MainActivity extends AppCompatActivity {
     public void FMBSave(View view) {
         Log.d("Noter", "button clicked");
         Toast.makeText(this, "button clicked", Toast.LENGTH_LONG).show();
+        if(uri != null) {
+            EditText MEdit = findViewById(R.id.MEdit);
+            byte[] byteArrray = MEdit.getText().toString().getBytes();
+            Log.d("Noter byte Array", byteArrray.toString());
+
+            String p = uri.getPath(); // /document/raw:/storage/emulated/0/Download/t.txt
+            String HC = "/storage/emulated/0/Download/HC.txt"; //test hardcode
+
+            try {
+            //FileOutputStream fout = new FileOutputStream(p);
+            //requestPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, 1);
+            FileOutputStream fout = new FileOutputStream(HC); //permission denied
+            fout.write(byteArrray);
+            fout.close(); } catch(Exception e) {Log.d("Noter", e.getLocalizedMessage());}
+        }
     }
 }
