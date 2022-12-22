5af3e5194cd1a6df4a267a0a5615612909d3abdc
twisted rainbow on fire <43458794+x1sts@users.noreply.github.com>
2022-12-22 16:26:32 +1000

    release 1.2 signed apk needs google play, back to debug apk release. added ask for storage permission dialog. share to had redundant code. singleInstance, want to add append or open dialog. my main filemanager+ on note 8.0 phone gave different path than emulator. Ui size changes.

--- app/src/main/java/com/psymblyb/noter/MainActivity.java
+++ app/src/main/java/com/psymblyb/noter/MainActivity.java
@@ -12,6 +12,10 @@ permission grant Dialog
 
 package com.psymblyb.noter;
 
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.ClipData;
 import android.content.ClipDescription;
@@ -19,6 +23,7 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -36,16 +41,34 @@ public class MainActivity extends AppCompatActivity {
     private static String type;
     private static Uri uri;
     private static String p;
+    private static String permission = WRITE_EXTERNAL_STORAGE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("Noter version", "Actions");
-        Toast.makeText(this, "V: Actions", Toast.LENGTH_LONG).show();
+        Log.i("Noter version", "Permission");
+        Toast.makeText(this, "V: Permission", Toast.LENGTH_LONG).show();
 
         //find xml components
         EditText MEdit = findViewById(R.id.MEdit);
+
+//        // register permission dialog
+        ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult
+                (
+                        new ActivityResultContracts.RequestPermission(), isGranted -> {
+                            if (isGranted) {
+                                //Log.d("Noter request", " is");
+                            } else {
+                                //Log.d("Noter request", " isnt");
+                            }
+                        }
+                );
+//        // 4 permission already granted to your app?
+//        //if (ContextCompat.checkSelfPermission(this, ACCESS_MEDIA_LOCATION) == PERMISSION_DENIED)
+            requestPermissionLauncher.launch(permission);
+
+
 
         // initialise intent
         Intent intent = getIntent();
@@ -53,56 +76,62 @@ public class MainActivity extends AppCompatActivity {
         type = intent.getType();
 
         // if opened directly
-        if(Intent.ACTION_VIEW.equals(action)) {
+        if (Intent.ACTION_VIEW.equals(action)) {
             try {
                 uri = intent.getData();
                 BufferedReader br = new BufferedReader(new
-                InputStreamReader(getContentResolver().openInputStream(uri), StandardCharsets.UTF_8));
+                        InputStreamReader(getContentResolver().openInputStream(uri), StandardCharsets.UTF_8));
                 int I;
-                while( (I = br.read() ) != -1) {
-                    SharedText = SharedText + (char)I;
+                while ((I = br.read()) != -1) {
+                    SharedText = SharedText + (char) I;
                 }
             } catch (Exception e) {
-                Log.d("Noter open view", e.getLocalizedMessage()); }
+                Log.d("Noter open view", e.getLocalizedMessage());
+            }
         }
 
         // if opened with shared file
-        if( intent.hasCategory("android.intent.category.DEFAULT") ){
-            Log.d("Noter", "If Shared file");
-            Toast.makeText(this, "Opened shared file", Toast.LENGTH_LONG).show();
+        //if (intent.hasCategory("android.intent.category.DEFAULT")) {
+        if (Intent.ACTION_SEND.equals(action)) {
+            //Toast.makeText(this, "Opened shared file", Toast.LENGTH_LONG).show();
 
-            Log.d("Noter Shared Intent", intent.toString());
-            if(intent.getClipData() != null)
-            {
-                SharedText = intent.getClipData().getItemAt(0).coerceToText(this).toString();
-                Log.d("Noter SharedText", "Shared Text");
-            }
             // if shared from text from share highlighted text ++
-            if (intent.getStringExtra(Intent.EXTRA_TEXT) != null) {
-                SharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
-                Log.d("Noter", "Highlighted text");
+//            if (intent.getStringExtra(Intent.EXTRA_TEXT) != null) {
+//                SharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+//                Log.d("Noter extra", SharedText);
+//            }
+
+            //AlertDialog(this);
+
+            // actually this handles both
+            Log.d("Noter Shared Intent", intent.toString());
+            if (intent.getClipData() != null) {
+                SharedText = SharedText + intent.getClipData().getItemAt(0).coerceToText(this).toString();
+                Log.d("Noter clip", SharedText);
             }
+
         }
 
-        Log.d("Noter Final null", String.valueOf(SharedText==null));
-        if (SharedText != null)
-        {
+        //Log.d("Noter Final null", String.valueOf(SharedText == null));
+        if (SharedText != null) {
             MEdit.setText(SharedText); // set to textbox
             SharedText = "";
         }
     }
 
-    //needs to be static, belongs to class not an object
-    public static boolean validateType() { return type==null; }
-
     public void FMBSave(View view) {
-        if(uri != null) {
-            p = uri.getLastPathSegment().substring(4); // remove raw: prefix to get corect save path
+
+        if (uri != null) {
+            if ( Build.FINGERPRINT.contains("generic") ) p = uri.getLastPathSegment().substring(4); // remove raw: on my files emulator
+            else {
+                p = uri.getPath().substring(5); // remove root prefix filemanager+
+                //my files on samsung external/file/69367 wtf
+            }
+
             Log.d("Noter path substring", p);
-        }
-        else {
+        } else {
             // add overwrite dialog
-            Log.d("Noter error", "reach" );
+            Log.d("Noter error", "reach");
         }
 
         try {
@@ -112,8 +141,12 @@ public class MainActivity extends AppCompatActivity {
             //requestPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, 1); // start permission request Activity, not working did it manually
             FileOutputStream fout = new FileOutputStream(p);
             fout.write(byteArrray);
-            fout.close(); } catch(Exception e) {Log.d("Noter error", e.getLocalizedMessage());}
-        finally { Toast.makeText(this, "Saved File", Toast.LENGTH_LONG).show(); }
+            fout.close();
+        } catch (Exception e) {
+            Log.d("Noter error", e.getLocalizedMessage());
+        } finally {
+            Toast.makeText(this, "Saved File", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void FMBUndo(View view) {
@@ -121,6 +154,7 @@ public class MainActivity extends AppCompatActivity {
         m.onTextContextMenuItem(android.R.id.undo);
 
     }
+
     public void FMBRedo(View view) {
         EditText m = findViewById(R.id.MEdit);
         m.onTextContextMenuItem(android.R.id.redo);
@@ -129,11 +163,12 @@ public class MainActivity extends AppCompatActivity {
     public void FMBPaste(View view) {
         ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
         String pasteData = "";
-        if(clipboard.getPrimaryClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
+        if (clipboard.getPrimaryClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
             ClipData.Item item = clipboard.getPrimaryClip().getItemAt(0);
-            if(item != null) pasteData = item.getText().toString();
+            if (item != null) pasteData = item.getText().toString();
         }
         EditText e = findViewById(R.id.MEdit);
         e.setText(e.getText() + pasteData);
     }
+
 }
